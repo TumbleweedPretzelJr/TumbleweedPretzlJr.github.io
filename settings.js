cf6acb662f01cfac32a70dc7c14ccf5500b06af2
TumbleweedPretzelJr <jhandarfsk@gmail.com>
2025-05-07 00:33:45 -0700

    VS Map

---
+++ settings.js
@@ -0,0 +1,8 @@
+var settings = {
+    title: "My VS Server",
+    titleBarName: "My VS",
+    titleBarCommunity: "My Community",
+    siteUrl: "https://example.com",
+    updateText : "Last updated on ",
+    lastUpdated : "Wednesday, May 7, 2025."
+}
